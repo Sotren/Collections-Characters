df245f19f4731107444b48f26a1c7fb4814cd65a
soten <sotekrg@gmail.com>
2022-08-24 19:45:08 +0700

    Work on selected actor start

--- TestGit/Flow/SelectedCharacter/ActorSelectedViewModel.swift
+++ TestGit/Flow/SelectedCharacter/ActorSelectedViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+ 
+class ActorSelectedViewModel {
+    
+    var char_id: Int!
+    let actor:Observable<Character?> = Observable(nil)
+    func requestData() {
+        guard let id = char_id else { return }
+        CharacterManager.shared.fetchData(from: Networking.urlString + String(id)) { actorData  in
+            return  self.actor.value = actorData.first
+        }
+    }
+//   private func test () -> Character {
+//        CharacterManager.shared.fetchData(from: Networking.urlString + String(char_id)) { actorData in
+//            let actor = actorData.first
+//            return actor
+//        }
+//    }
+}
+
--- TestGit/Flow/SelectedCharacter/SelectedCharacterViewController.swift
+++ TestGit/Flow/SelectedCharacter/SelectedCharacterViewController.swift
@@ -10,6 +10,7 @@ import Kingfisher
 
 class SelectedCharacterViewController: UIViewController {
     
+    let viewModel = ActorSelectedViewModel()
     var char_id: Int!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameCharacter: UILabel!
@@ -21,25 +22,33 @@ class SelectedCharacterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestData()
+        setupBinders()
+        viewModel.requestData()
     }
     
-    private func requestData() {
-        guard let id = char_id else { return }
-        CharacterManager.shared.fetchData(from: Networking.urlString + String(id)) { actorData  in
-            guard let actor = actorData.first else { return }
-            self.setActorData(actorData: actor)
+    func setupBinders() {
+        viewModel.actor.bind { [self ] actor in
+            if let actor = actor {
+                self.nameCharacter.text = "Имя актера: " + actor.name
+                let url = URL(string: "\(actor.img)")
+                self.imageView.kf.setImage(with: url)
+                self.category.text = "Категория: " + actor.category
+                self.portrayed.text = "Играет роль: " + actor.portrayed
+                self.nickName.text = "Прозвище: " + actor.nickname
+                self.status.text = "Статус: " + actor.status
+                self.birthday.text = "Дата рождения: " + actor.birthday
+            }
         }
-    }
-    
-    private func setActorData (actorData: Character) {
-        nameCharacter.text = "Имя актера: " + actorData.name
-        let url = URL(string: "\(actorData.img)")
-        imageView.kf.setImage(with: url)
-        category.text = "Категория: " + actorData.category
-        portrayed.text = "Играет роль: " + actorData.portrayed
-        nickName.text = "Прозвище: " + actorData.nickname
-        status.text = "Статус: " + actorData.status
-        birthday.text = "Дата рождения: " + actorData.birthday
-    }
+//
+//    private func setActorData (actorData: Character) {
+//        nameCharacter.text = "Имя актера: " + actorData.name
+//        let url = URL(string: "\(actorData.img)")
+//        imageView.kf.setImage(with: url)
+//        category.text = "Категория: " + actorData.category
+//        portrayed.text = "Играет роль: " + actorData.portrayed
+//        nickName.text = "Прозвище: " + actorData.nickname
+//        status.text = "Статус: " + actorData.status
+//        birthday.text = "Дата рождения: " + actorData.birthday
+//    }
+}
 }
